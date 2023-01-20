45eefa8849ce6055a7766516dcb68c465ab57647
yurii <yur4ik322@gmail.com>
2023-01-20 16:19:09 +0200

    second comm

--- test_task/test_task/spiders/manoloblahnik.py
+++ test_task/test_task/spiders/manoloblahnik.py
@@ -1,3 +1,4 @@
+import json
 import pdb
 
 import scrapy
@@ -8,8 +9,24 @@ class ManoloblahnikSpider(scrapy.Spider):
     name = 'manoloblahnik'
     allowed_domains = ['manoloblahnik.com']
     start_urls = ['https://www.manoloblahnik.com/gb']
+    cookies = {
+        'auto_country_switcher': 'gb',
+        'newsletter-holded': 'true',
+        'X-Magento-Vary': 'f4779982487cd2ac4b16ccb8c340d6e66ae2d049',
+    }
+
+    headers = {
+        'authority': 'www.manoloblahnik.com',
+        'accept': '*/*',
+        'accept-language': 'uk-UA,uk;q=0.9,en-US;q=0.8,en;q=0.7',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    }
 
     def parse(self, response):
+        req = scrapy.Request("https://www.manoloblahnik.com/gb/category/index/index?p=3&id=57", callback=self.one_page_cat, headers=self.headers, cookies=self.cookies, dont_filter=True)
+
+        yield req
+        pdb.set_trace()
         print('ABIBA')
         top_lvl = response.css('.level0.level-top')
         for par in top_lvl:
@@ -30,6 +47,8 @@ class ManoloblahnikSpider(scrapy.Spider):
             cat_name = sub_cat.css('h2 a span::text').get()
             cat_url = sub_cat.css('h2 a::attr(href)').get()
             cat_id = scrapy.Request(cat_url, callback=self.parse_cat_id)
+            if not cat_id:
+                self.one_page_cat(cat_id.response)#???
             print(f"Cat name {cat_name}, Cat url {cat_url}, Cat id {cat_id}")
 
             print('NEXT_NEXT LVL')
@@ -41,14 +60,39 @@ class ManoloblahnikSpider(scrapy.Spider):
 
 
     def parse_cat_id(self, response):
+        load_more = response.css('#load-more::attr(data-url)').get()
+        if not load_more:
+            return None
         cat_id = urlparse(response.css('#load-more::attr(data-url)').get()).query.split('id=')[-1]
-        print(cat_id)
-        response.meta['id'] = cat_id
-        return response.meta['id']
+        return cat_id
 
 
-    def get_prod_url(self, cat_id):
-        return f"https://www.manoloblahnik.com/gb/category/index/index?p=2&id={cat_id}"
+
+    def get_prod_url(self, cat_id, start=2):
+        return f"https://www.manoloblahnik.com/gb/category/index/index?p={start}&id={cat_id}"
 
     def get_product(self):
         pass
+
+    def one_page_cat(self, response):
+        self.logger.info("Visited %s", response.url)
+        print('start_scrap_product')
+        # prod_pull = response.css('.product-details')
+        # pdb.set_trace()
+        # for script in response.css('script').getall():
+        #     if "ecommerce" in script:
+        #         # start = '= [{'
+        #         # end = '"}];'
+        #         # product_dict = json.loads(script[script.rfind(start)+2:script.rfind(end)+3])
+        product_pool =response.css('.product.show-me')
+        for product in product_pool:
+            product_url = product.css('a::attr(href)').get()
+            product_info = product.css('a::attr(onclick)').get()
+            product_info = json.loads(product_info[product_info.rfind('{"name"'):product_info.rfind('"}]}')+2])
+            product_name = product_info['name']
+            product_id =product_info['id']
+            product_price =product_info['price']
+            product_category = product_info['category']
+            product_img = product.css('.photo.image-hover source[type="image/jpeg"]::attr(data-isrcset)').get()
+            print(f'Name {product_name} Url:{product_url}, Id {product_id}, price {product_price}, Category {product_category}, img {product_img}')
+            pdb.set_trace()
